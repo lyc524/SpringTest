9cec95446b609b586f3b575725622036fe7b8966
kavi <kavilee@163.com>
2018-01-31 18:12:36 +0800

    init code

---
+++ src/com/springtest/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.springtest;
+
+public class HelloWorld {
+	 private String message;
+	   public void setMessage(String message){
+	      this.message  = message;
+	   }
+	   public void getMessage(){
+	      System.out.println("Your Message 123: " + message);
+	   }
+}
